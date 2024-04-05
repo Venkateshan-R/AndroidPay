97311edec4e513fd4c1017bc57b1c10b8452f292
VenkateshanR <venkateshan.r@sunnetwork.in>
2024-04-05 16:21:06 +0530

    back click events handled

--- app/src/main/java/com/example/androidpay/ui/utils/Constants.kt
+++ app/src/main/java/com/example/androidpay/ui/utils/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     const val MOBILE_NUMBER_LENGTH = 10
     const val ACCOUNT_NUMBER_LENGTH = 12
     const val AUTHENTICATION_PIN = 4
-    const val PER_TRANSACTION_LIMIT :Double= 50000.0
-    const val PER_DAY_TRANSACTION_LIMIT :Double= 100000.0
+    const val PER_TRANSACTION_LIMIT :Double= 50000.00
+    const val PER_DAY_TRANSACTION_LIMIT :Double= 100000.00
 
 }
